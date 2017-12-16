add40c153fd4bb999f02962abb72dd3e35d2b5d0
miikargh <mamoilanen@gmail.com>
2017-12-16 09:01:13 +0200

    Addded a function for IBM NLU Analysis

---
+++ src/analyze.js
@@ -0,0 +1,32 @@
+const user = require('../secrets/secrets.json').NLUUsername
+const pswd = require('../secrets/secrets.json').NLUPassword
+const NLUV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+
+const nlu = new NLUV1({
+  'username': user,
+  'password': pswd,
+  'version_date': '2017-02-27'
+})
+
+const params = {
+  features: {
+    entities: {
+      emotion: true,
+      sentiment: true,
+      limit: 2
+    },
+    keywords: {
+      emotion: true,
+      sentiment: true,
+      limit: 2
+    }
+  }
+}
+
+module.exports = (text) => new Promise((resolve, reject) => {
+  nlu.analyze(Object.assign(params, {text: text}), (err, res) => {
+    if (err) reject(err)
+    else
+      resolve(JSON.stringify(res, null, 2))
+  })
+})
