d3592b0431e8e4063126fb844a0c5ff81d99143f
miikargh <mamoilanen@gmail.com>
2018-02-03 08:00:12 +0200

    Cleaned up the return JSON.

--- src/apis/analyze.js
+++ src/apis/analyze.js
@@ -1,5 +1,5 @@
-const user = require('../secrets/secrets.json').IBM.NLUUsername
-const pswd = require('../secrets/secrets.json').IBM.NLUPassword
+const user = require('../../secrets/secrets.json').IBM.NLUUsername
+const pswd = require('../../secrets/secrets.json').IBM.NLUPassword
 const NLUV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
 
 const nlu = new NLUV1({
@@ -15,11 +15,36 @@ const params = {
   }
 }
 
-module.exports = (text) => new Promise((resolve, reject) => {
-  nlu.analyze(Object.assign(params, {text: text}), (err, res) => {
-    //err ? reject(err) : resolve(JSON.stringify(res, null, 2))
-    if (err) reject(err)
-    else
-      resolve(JSON.stringify(res, null, 2))
+/**
+ * @typedef {Object} analysis
+ * @property {Number} sentiment – Sentiment score ranging from -1 to 1
+ * @property {Object} emotion – Emotion scores from 0 to 1
+ * @property {Number} emotion.sadness
+ * @property {Number} emotion.joy
+ * @property {Number} emotion.fear
+ * @property {Number} emotion.disgust
+ * @property {Number} emotion.anger
+ * 
+ */
+
+/**
+ * Analyzes given text for sentiment and emotion.
+ *
+ * @param {String} text – Text to be analyzed
+ * @returns {Object.<analysis>} – JSONifiable analysis object
+ */
+module.exports = (text) =>
+  new Promise((resolve, reject) => {
+    console.log('analyzing')
+    nlu.analyze(Object.assign(params, {text: text}), (err, res) => {
+      err
+        ? reject(err)
+        : resolve(res)
+    })
   })
-})
+  .then(res => {
+    return {
+      sentiment: res.sentiment.document.score,
+      emotion: res.emotion.document.emotion
+    }
+  })
